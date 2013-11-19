8f010cac5728f7c12201947698060d32de543320
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-19 17:17:03 -0500

    start with temperature example

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,85 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// (c) 2013 Don Coleman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global mainPage, deviceList, refreshButton */
+/* global detailPage, tempFahrenheit, tempCelsius, closeButton */
+/* global rfduino, alert */
+'use strict';
+
+var arrayBufferToFloat = function (ab) {
+    var a = new Float32Array(ab);
+    return a[0];
+};
+
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
+        detailPage.hidden = true;
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
+        closeButton.addEventListener('touchstart', this.disconnect, false);
+        deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.refreshDeviceList();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    refreshDeviceList: function() {
+        deviceList.innerHTML = ''; // empties the list
+        rfduino.discover(5, app.onDiscoverDevice, app.onError);
+    },
+    onDiscoverDevice: function(device) {
+        var listItem = document.createElement('li'),
+            html = '<b>' + device.name + '</b><br/>' +
+                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+                'Advertising: ' + device.advertising + '<br/>' +
+                device.uuid;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        listItem.setAttribute('uuid', device.uuid);
+        listItem.innerHTML = html;
+        deviceList.appendChild(listItem);
+    },
+    connect: function(e) {
+        var uuid = e.target.getAttribute('uuid'),
+            onConnect = function() {
+                rfduino.onData(app.onData, app.onError);
+                app.showDetailPage();
+            };
 
-        console.log('Received Event: ' + id);
+        rfduino.connect(uuid, onConnect, app.onError);
+    },
+    onData: function(data) {
+        console.log(data);
+        var celsius = arrayBufferToFloat(data),
+            fahrenheit = celsius * 1.8 + 32;
+
+        tempCelsius.innerHTML = celsius.toFixed(2);
+        tempFahrenheit.innerHTML = fahrenheit.toFixed(2);
+    },
+    disconnect: function() {
+        rfduino.disconnect(app.showMainPage, app.onError);
+    },
+    showMainPage: function() {
+        mainPage.hidden = false;
+        detailPage.hidden = true;
+    },
+    showDetailPage: function() {
+        mainPage.hidden = true;
+        detailPage.hidden = false;
+    },
+    onError: function(reason) {
+        alert(reason); // real apps should use notification.alert
     }
 };
